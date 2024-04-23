bcae6750f95277533fc7574ec0d08e2a4f2b7064
keanu-a <keanucaloua@gmail.com>
2024-04-23 01:19:21 -0700

    Got dataset class to work

---
+++ dataset.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+from torch.utils.data import Dataset
+from torch import from_numpy
+from PIL import Image
+
+
+class CityscapesDataset(Dataset):
+    def __init__(self, image_dir, label_dir, split, transform=None):
+        self.image_dir = image_dir
+        self.label_dir = label_dir
+        self.split = split # "train", "val", or "test"
+        self.transform = transform
+        self.image_paths = []
+        self.label_paths = []
+
+        # Load image and label paths based on split
+        self._load_paths()
+
+
+    # Iterates through directories based on the split
+    def _load_paths(self):
+        # Go to specfied split directory and image file paths to image_paths
+        for dirpath, _, filenames in os.walk(os.path.join(self.image_dir, self.split)):
+            for filename in filenames:
+                if filename.endswith(".png"):
+                    image_path = os.path.join(dirpath, filename)
+
+                    # Only using labelIds since doing semantic segmentation
+                    label_dirpath = dirpath.replace("leftImg8bit", "gtFine")
+                    label_filename = filename.replace("_leftImg8bit.png", "_gtFine_labelIds.png")
+                    label_path = os.path.join(label_dirpath, label_filename)
+
+                    self.image_paths.append(image_path)
+                    self.label_paths.append(label_path)
+
+    
+    # Gets length of the dataset
+    def __len__(self):
+        return len(self.image_paths)
+    
+
+    # Gets a specific image-label based on index
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        label_path = self.label_paths[idx]
+
+        image = Image.open(image_path).convert("RGB")
+        label = Image.open(label_path).convert("L")
+
+        if self.transform:
+            image = self.transform(image)
+            label = self.transform(label)
+
+        return image, label
--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-from torchvision.datasets import Cityscapes
+import os
 import torch
-from matplotlib import pyplot as plt
-import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from dataset import CityscapesDataset
+import matplotlib.pyplot as plt
+
+CITYSCAPES_PATH = os.path.join("cityscapes")
+IMAGE_DIR = os.path.join(CITYSCAPES_PATH, "leftImg8bit")
+LABEL_DIR = os.path.join(CITYSCAPES_PATH, "gtFine")
+
+BATCH_SIZE = 8 # Number of images to be processed together during training
+IMAGE_SIZE = (256, 256) # Size for resizing images for training
+CLASS_AMOUNT = 30 # Cityscapes has 30 classes
+
+transform = transforms.Compose([
+    transforms.Resize(IMAGE_SIZE), # Resizing for training
+    transforms.ToTensor() # Converting to PyTorch tensors
+])
+
+# Creating training and validation datasets
+train_dataset = CityscapesDataset(image_dir=IMAGE_DIR, label_dir=LABEL_DIR, split="train", transform=transform)
+
+image, label = train_dataset[0]
+
+to_pil = transforms.ToPILImage()
+image_pil = to_pil(image)
+label_pil = to_pil(label)
+
+# Display the image and label
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Image")
+plt.imshow(image_pil)
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.title("Label")
+plt.imshow(label_pil, cmap='gray')
+plt.axis('off')
+
+plt.show()
